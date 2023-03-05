bc1b9b73d928f8c16d0a321927803d8b4a815ae1
pwamly <pwamly2016@gmail.com>
2023-03-06 00:16:42 +0300

    added some types

--- meterui/src/redux/store.ts
+++ meterui/src/redux/store.ts
@@ -1,23 +1,62 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+// ============ types =============
+
+interface ProfileType{
+    fullname: string,
+    userId: string,
+    language: string,
+    location: string,
+    title: string,
+    mobile: string,
+    email: string,
+    editProfile: boolean
+}
+
+interface accountType {
+    accountName: string,
+    balance: string,
+    dateCreated: string,
+    location: string,
+    editAccount: boolean
+}
+
+// ============ store =============
+
+const ProfileValue = {
+    fullname: '',
+    userId: '',
+    language: '',
+    location: '',
+    title: '',
+    mobile: '',
+    email: '',
+    editProfile: false
+} as ProfileType
+
+const accountValue = {
+    accountName: '',
+    balance: '',
+    dateCreated: '',
+    location: '',
+    editAccount: false
+} as accountType
+
+interface initialStateProfileType{
+    ProfileValue:ProfileType
+}
+
+interface initialStateAccountType{
+    accountValue:accountType
+}
 
 const userSlice = createSlice({
     name: 'profile',
-    initialState: {
-            fullname: '',
-            userId: '',
-            language: '',
-            location: '',
-            title: '',
-            mobile: '',
-            email: '',
-            editProfile: false
-        
-    },
+    initialState: { ProfileValue },
     reducers: {
-        editProfile: (state) => {
-            state.editProfile = !state.editProfile;
-            
+        editProfile: (state:initialStateProfileType) => {
+            state.ProfileValue.editProfile = !state.ProfileValue.editProfile;
+
         }
 
     }
@@ -26,31 +65,28 @@ const userSlice = createSlice({
 
 const accountSlice = createSlice({
     name: 'account',
-    initialState: {
-            accountName: '',
-            balance: '',
-            dateCreated: '',
-            location: '',
-            editAccount: false
-        
-    },
+    initialState: { accountValue },
     reducers: {
-        editAccount: (state,action) => {
-            state.accountName = action.payload;
-            
+        editAccount: (state:initialStateAccountType, action:PayloadAction<string>) => {
+            state.accountValue.accountName = action.payload;
+
         }
 
     }
 
 });
 
+// ============ reducers =============
+
 export const { editProfile } = userSlice.actions;
 export const { editAccount } = accountSlice.actions;
 
 
+// ============ stores =============
+
 export const store = configureStore({
-    reducer: { 
+    reducer: {
         profile: userSlice.reducer,
         account: accountSlice.reducer
-     }
+    }
 });
